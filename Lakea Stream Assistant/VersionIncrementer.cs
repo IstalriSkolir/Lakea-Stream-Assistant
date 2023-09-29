04e9bf3a7eab136e3da14fd3a4ecc9f96658adb5
Istalri Skolir Productions <johnthewolfson@gmail.com>
2023-09-29 09:31:36 +0100

    #138 Battle Simulator Level Check
    - Lakea checks a Characters level before running the Battle Simulator
    - Lakea checks the Battle Simulator exit code after it closes

--- Lakea Stream Assistant/VersionIncrementer.cs
+++ Lakea Stream Assistant/VersionIncrementer.cs
@@ -1,7 +1,7 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.3.26.414")]
-[assembly: AssemblyFileVersion("0.3.26.414")]
-[assembly: AssemblyInformationalVersion("0.3.26.414")]
+[assembly: AssemblyVersion("0.3.26.500")]
+[assembly: AssemblyFileVersion("0.3.26.500")]
+[assembly: AssemblyInformationalVersion("0.3.26.500")]
 
