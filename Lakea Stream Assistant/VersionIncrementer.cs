97f30c8a26b9677d344bec4d19c17c12facce792
Istalri Skolir Productions <johnthewolfson@gmail.com>
2023-09-30 18:33:05 +0100

    #140 Battle Simulator Queue
    - Lakea uses a queuing system for the Battle Simulator

--- Lakea Stream Assistant/VersionIncrementer.cs
+++ Lakea Stream Assistant/VersionIncrementer.cs
@@ -1,7 +1,7 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.3.27.828")]
-[assembly: AssemblyFileVersion("0.3.27.828")]
-[assembly: AssemblyInformationalVersion("0.3.27.828")]
+[assembly: AssemblyVersion("0.3.27.1051")]
+[assembly: AssemblyFileVersion("0.3.27.1051")]
+[assembly: AssemblyInformationalVersion("0.3.27.1051")]
 
