6ace80d82de4baa7b0d3d64353fbaed9ef0890f5
Istalri Skolir Productions <johnthewolfson@gmail.com>
2023-10-01 12:50:47 +0100

    #30 Callback Loop Bug
    - Config Validation Updated
    - Looping bug still not fixed

--- Lakea Stream Assistant/VersionIncrementer.cs
+++ Lakea Stream Assistant/VersionIncrementer.cs
@@ -1,7 +1,7 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.3.28.534")]
-[assembly: AssemblyFileVersion("0.3.28.534")]
-[assembly: AssemblyInformationalVersion("0.3.28.534")]
+[assembly: AssemblyVersion("0.3.28.608")]
+[assembly: AssemblyFileVersion("0.3.28.608")]
+[assembly: AssemblyInformationalVersion("0.3.28.608")]
 
