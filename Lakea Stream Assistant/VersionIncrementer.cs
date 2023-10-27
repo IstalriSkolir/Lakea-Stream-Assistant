38ef3e4750854f8376862e9ec5c638620f351103
Istalri Skolir Productions <johnthewolfson@gmail.com>
2023-10-27 20:02:38 +0100

    Hotfix

--- Lakea Stream Assistant/VersionIncrementer.cs
+++ Lakea Stream Assistant/VersionIncrementer.cs
@@ -1,7 +1,7 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.3.52.1081")]
-[assembly: AssemblyFileVersion("0.3.52.1081")]
-[assembly: AssemblyInformationalVersion("0.3.52.1081")]
+[assembly: AssemblyVersion("0.3.54.1071")]
+[assembly: AssemblyFileVersion("0.3.54.1071")]
+[assembly: AssemblyInformationalVersion("0.3.54.1071")]
 
