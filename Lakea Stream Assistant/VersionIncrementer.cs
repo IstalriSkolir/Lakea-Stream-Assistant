014d40b5ac5f9f33991c27bb0be1c0f96463a290
Istalri Skolir Productions <johnthewolfson@gmail.com>
2023-12-16 13:39:28 +0000

    New Build Deployed
    - Version number updated

--- Lakea Stream Assistant/VersionIncrementer.cs
+++ Lakea Stream Assistant/VersionIncrementer.cs
@@ -1,6 +1,7 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.3.98.789")]
-[assembly: AssemblyFileVersion("0.3.98.789")]
-[assembly: AssemblyInformationalVersion("0.3.98.789")]
+[assembly: AssemblyVersion("0.4.0.818")]
+[assembly: AssemblyFileVersion("0.4.0.818")]
+[assembly: AssemblyInformationalVersion("0.4.0.818")]
+
