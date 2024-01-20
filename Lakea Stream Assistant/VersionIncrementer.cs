0fb2f194739c94cc3026c2680e72988a0f1cb110
Istalri Skolir Productions <johnthewolfson@gmail.com>
2024-01-20 12:38:43 +0000

    Deploying version 0.5.0

--- Lakea Stream Assistant/VersionIncrementer.cs
+++ Lakea Stream Assistant/VersionIncrementer.cs
@@ -1,7 +1,7 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.4.35.742")]
-[assembly: AssemblyFileVersion("0.4.35.742")]
-[assembly: AssemblyInformationalVersion("0.4.35.742")]
+[assembly: AssemblyVersion("0.5.0.757")]
+[assembly: AssemblyFileVersion("0.5.0.757")]
+[assembly: AssemblyInformationalVersion("0.5.0.757")]
 
