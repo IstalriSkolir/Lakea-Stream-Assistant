8d737e1ccf93c6221f03e82fc7b01b1bf9768ccb
Istalri Skolir Productions <johnthewolfson@gmail.com>
2024-01-23 17:16:34 +0000

    Hotfixes
    - Decreased Training Speed
    - Fixed Python Arguments

--- Lakea Stream Assistant/VersionIncrementer.cs
+++ Lakea Stream Assistant/VersionIncrementer.cs
@@ -1,7 +1,7 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.5.0.757")]
-[assembly: AssemblyFileVersion("0.5.0.757")]
-[assembly: AssemblyInformationalVersion("0.5.0.757")]
+[assembly: AssemblyVersion("0.5.3.1034")]
+[assembly: AssemblyFileVersion("0.5.3.1034")]
+[assembly: AssemblyInformationalVersion("0.5.3.1034")]
 
