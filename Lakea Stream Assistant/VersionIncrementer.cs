92ed86e37ca8996611db037e9cdd45bb27dc6476
Istalri Skolir Productions <47321091+IstalriSkolir@users.noreply.github.com>
2024-08-11 14:35:04 +0100

    #260 Lakea Exit Events Fixed
    - Lakea will now wait for all exit events to complete before closing

--- Lakea Stream Assistant/VersionIncrementer.cs
+++ Lakea Stream Assistant/VersionIncrementer.cs
@@ -1,7 +1,7 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.7.65.804")]
-[assembly: AssemblyFileVersion("0.7.65.804")]
-[assembly: AssemblyInformationalVersion("0.7.65.804")]
+[assembly: AssemblyVersion("0.7.65.814")]
+[assembly: AssemblyFileVersion("0.7.65.814")]
+[assembly: AssemblyInformationalVersion("0.7.65.814")]
 
