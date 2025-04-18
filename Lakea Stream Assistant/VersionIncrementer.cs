382783e76cbdc55e6119af6d19052e00fa9553a1
Istalri Skolir Productions <47321091+IstalriSkolir@users.noreply.github.com>
2025-04-18 14:56:22 +0100

    #316 Top Bits Command
    - Added top bits command
    - Fixed totalbits command configuration
    - Added Lakea's capture Excel sheet

--- Lakea Stream Assistant/VersionIncrementer.cs
+++ Lakea Stream Assistant/VersionIncrementer.cs
@@ -1,7 +1,7 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.10.0.760")]
-[assembly: AssemblyFileVersion("0.10.0.760")]
-[assembly: AssemblyInformationalVersion("0.10.0.760")]
+[assembly: AssemblyVersion("0.10.5.534")]
+[assembly: AssemblyFileVersion("0.10.5.534")]
+[assembly: AssemblyInformationalVersion("0.10.5.534")]
 
