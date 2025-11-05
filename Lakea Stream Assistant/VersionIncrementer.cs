00a0168709a49ded374575e18339d0a2ed4dc4af
Istalri Skolir Productions <47321091+IstalriSkolir@users.noreply.github.com>
2025-11-05 23:07:42 +0000

    #326 Top Bits Template
    - Top bits template has been added
    - Defaults to 10, can be changed with Count argument

--- Lakea Stream Assistant/VersionIncrementer.cs
+++ Lakea Stream Assistant/VersionIncrementer.cs
@@ -1,7 +1,7 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.10.206.1096")]
-[assembly: AssemblyFileVersion("0.10.206.1096")]
-[assembly: AssemblyInformationalVersion("0.10.206.1096")]
+[assembly: AssemblyVersion("0.10.206.1384")]
+[assembly: AssemblyFileVersion("0.10.206.1384")]
+[assembly: AssemblyInformationalVersion("0.10.206.1384")]
 
